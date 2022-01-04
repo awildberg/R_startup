d5c632f681b715f6b1efed7223699306fe60a843
awildberg <77085223+awildberg@users.noreply.github.com>
2022-01-04 23:37:20 +0100

    Update sample_rng.jl

--- sample_rng.jl
+++ sample_rng.jl
@@ -1,9 +1,20 @@
-function sample_rng(start::Int, en::Int; replace=false)
+function sample_rng(start::Int, en::Int; len=0, replace=false)::Vector{Int}
+  if start > en
+    error(string("Start ", start, " > End ", en))
+  end
+  if replace == false && len > (en-start)
+    error(string("Return length larger than range of input ", (en-start) ," < ", en))
+  end
   vec = collect(start:en)
   indx = collect(1:length(vec))
+  rep = (en - start)+1
+  if len > rep
+    rep = len
+  end
+
   rtn = []
 
-  for _ in vec
+  for _ in 1:rep
     n = length(indx)
     i = rand(1:n)
     push!(rtn, vec[indx[i]])
